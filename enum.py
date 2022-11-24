a963058a2ccaa6dd7c0e9a2adca80eec99a40081
v50n <vson.ng91@gmail.com>
2022-11-24 17:23:54 +0100

    Create enum.py

---
+++ enum.py
@@ -0,0 +1,8 @@
+import sys
+from colorama import Fore, init # for fancy colors, nothing else
+import subprocess
+
+def main():
+    host = sys.argv[1] # get agr 1 host
+    if host: 
+        subprocess.run()
