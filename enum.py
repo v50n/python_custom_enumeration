4eb9de6b2d2c5cd0498f2a89f79a744b95240a97
v50n <vson.ng91@gmail.com>
2022-11-24 21:15:07 +0100

    Update enum.py

--- enum.py
+++ enum.py
@@ -1,8 +1,25 @@
 import sys
 from colorama import Fore, init # for fancy colors, nothing else
 import subprocess
+import shlex # usefull for unix command
 
 def main():
     host = sys.argv[1] # get agr 1 host
     if host: 
-        subprocess.run()
+        print("===Nmap TCP all port is running===")
+        run_command(enum_command(nmap),host,outfile())
+        
+
+def run_command(command):
+    subprocess.run(shlex.split(command)) #subprocess.run get parameter as array and we need to split it by shlex. Shlex can support the space character like "User document"
+    
+def enum_command(c):
+    match c: 
+        case "nmap": 
+            return "nmap -sV -p-"
+    
+def outfile(outfile):
+    if outfile:
+        return ">" + outfile
+    else:
+        return "> enum_result.txt"
